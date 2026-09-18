f3924239921368a6d78f602c9ed198c270cd01c6
Ilham Hafizha <muhammadilhamhafizha@gmail.com>
2026-09-18 18:48:20 +0700

    refactor: migrate configuration to application.yml with profiles

--- src/main/java/com/hamfizh/bookcatalog/impl/GreetingServiceImpl.java
+++ src/main/java/com/hamfizh/bookcatalog/impl/GreetingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hamfizh.bookcatalog.impl;
 
+import com.hamfizh.bookcatalog.config.ApplicationProperties;
 import com.hamfizh.bookcatalog.service.GreetingService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -7,11 +8,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
-    @Value("${welcome.text}")
-    private String welcomeText;
+//    @Value("${welcome.text}")
+//    private String welcomeText;
+//
+//    @Value("${time-zone}")
+//    private String timezone;
+//
+//    @Value("${currency}")
+//    private String currency;
+    private ApplicationProperties applicationProperties;
+
+    public GreetingServiceImpl(ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
 
     @Override
     public String sayGreeing() {
-        return this.welcomeText;
+//        return this.welcomeText+" Your Time : " + timezone + ", Your Currency: " + currency;
+
+        return applicationProperties.getWelcomeText() +
+                " Your Time ZONE " + applicationProperties.getTimezone() +
+                " Your Currency " + applicationProperties.getCurrency();
     }
 }
